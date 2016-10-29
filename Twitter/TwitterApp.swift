464a0250ddb166d80e7f8b4cbdaaa892a50f405e
Carina Boo <cboo@cboo-macbookpro.roam.corp.google.com>
2016-10-28 17:06:45 -0700

    Add TwitterApp.swift with blank key and secret, so cloning git project is easier to get running later.

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Carina Boo on 10/28/16.
+//  Copyright © 2016 Carina Boo. All rights reserved.
+//
+
+import UIKit
+
+public class TwitterApp: NSObject {
+    public static let consumerKey = ""
+    public static let consumerSecret = ""
+}
